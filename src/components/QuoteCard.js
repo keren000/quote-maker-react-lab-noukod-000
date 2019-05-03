b9c4849f4011ddd3aba082cf93307159ccd33351
Keren-Happuc Fleurimond <keren@noukod.com>
2019-05-03 13:40:09 +0000

    Automatically backed up by Learn

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -26,8 +26,7 @@ const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) =>
           </button>
           <button
             type="button"
-            className="btn btn-danger" onClick={() => removeQuote(quote.id)}
-          >
+            className="btn btn-danger" onClick={() => removeQuote(quote.id)}>
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
