58bc34ef988ccaee8461912ddf8bdedcc68dfbb8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-05-03 13:39:48 +0000

    Automatically backed up by Learn

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -16,8 +16,7 @@ const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) =>
         <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
           <button
             type="button"
-            className="btn btn-primary" onClick={() => upvoteQuote(quote.id)}
-          >
+            className="btn btn-primary" onClick={() => upvoteQuote(quote.id)}>
             Upvote
           </button>
           <button
