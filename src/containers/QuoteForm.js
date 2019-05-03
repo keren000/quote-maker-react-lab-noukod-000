6733fd766b714e108201419b22c92091a66cf290
Keren-Happuc Fleurimond <keren@noukod.com>
2019-05-03 13:33:25 +0000

    Automatically backed up by Learn

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -53,6 +53,7 @@ class QuoteForm extends Component {
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
+                        name="content"
                         value={this.state.content}
                       />
                     </div>
